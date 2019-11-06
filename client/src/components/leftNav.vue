1369795e7ddb134cca3b139a55a0abcf26990b0b
togoc <49116563+togoc@users.noreply.github.com>
2019-11-07 00:55:53 +0800

    update

--- client/src/components/leftNav.vue
+++ client/src/components/leftNav.vue
@@ -3,26 +3,30 @@
     <el-row class="nav">
       <el-col>
         <el-menu
-          default-active=""
+          :default-active="path"
           class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-menu-item index="1">
-            <i class="el-icon-menu"></i>
-            <span slot="title"><router-link to="/form">资金管理</router-link></span>
-          </el-menu-item>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">人员管理</span>
-          </el-menu-item>
-          <el-menu-item index="3">
-            <i class="el-icon-setting"></i>
-            <span slot="title"><router-link to="/info">个人信息</router-link></span>
-          </el-menu-item>
+          <router-link to="/index">
+            <el-menu-item index="/index">
+              <i class="el-icon-menu"></i>
+              <span slot="title">首页</span>
+            </el-menu-item>
+          </router-link>
+          <router-link to="/form">
+            <el-menu-item index="/form">
+              <i class="el-icon-menu"></i>
+              <span slot="title">资金管理</span>
+            </el-menu-item>
+          </router-link>
+          <router-link to="/info">
+            <el-menu-item index="/info">
+              <i class="el-icon-setting"></i>
+              <span slot="title">个人信息</span>
+            </el-menu-item>
+          </router-link>
         </el-menu>
       </el-col>
     </el-row>
@@ -30,13 +34,11 @@
 </template>
 <script>
 export default {
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    }
+  data() {
+    return { path: "" };
+  },
+  created() {
+    this.path= this.$route.path
   }
 };
 </script>
@@ -63,8 +65,7 @@ export default {
 .el-menu {
   border: 0;
 }
-.nav{
-  border-top: 1px solid #1F2D3D;
+.nav {
+  border-top: 1px solid #1f2d3d;
 }
-
 </style>
