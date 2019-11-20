196a6fcd569304b942237f2c41d8356804d9a268
togoc <49116563+togoc@users.noreply.github.com>
2019-11-20 17:01:57 +0800

    update

--- server.js
+++ server.js
@@ -4,13 +4,25 @@ const port = process.env.USER ? 5000 : 5000
 const bodyparser = require('body-parser');
 const passport = require("passport")
 
+
+
 //history mode 防止刷新空白
-const history = require('connect-history-api-fallback');
-history({
-    index: '../public'
-  });
-app.use(history());
-app.use("/",express.static("public"))
+// const history = require('connect-history-api-fallback');
+// history({
+//     index: '../public'
+//   });
+// app.use(history());
+// app.use("/",express.static("public"))
+
+app.all('*', function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+  res.header("X-Powered-By",' 3.2.1')  
+  res.header("Content-Type", "application/json;charset=utf-8");  
+  next();  
+});
+
 
 app.use(passport.initialize())
 require("./parts/passport")(passport)
